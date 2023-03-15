f4498ac0cf5a5189ed4e0d0647b624961350c3e2
Aiga <aiga.andrejeva@gmail.com>
2023-03-15 23:50:14 +0000

    Style Game Rules modal to adjust different sizes of devices for responsiveness

---
+++ assets/script/index.js
@@ -0,0 +1,11 @@
+const showRules = document.getElementById('rules-modal-btn');
+const showRulesModal = document.getElementById('modal-game-rules');
+const closeRulesModal = document.getElementById('close-modal');
+
+showRules.addEventListener('click', () => {
+    showRulesModal.classList.add('show');
+});
+
+closeRulesModal.addEventListener('click', () =>{
+    showRulesModal.classList.remove('show');
+});
